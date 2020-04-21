3c5898432737e531a3ec02e4178e0ad0ca5c01b9
ForteScarlet <ForteScatlet@163.com>
2020-04-21 19:57:39 +0800

    章节1-引论

--- src/main/java/chapter1/引论.java
+++ src/main/java/chapter1/引论.java
@@ -14,9 +14,48 @@ public class 引论 {
      * 1.2.3 级数
      * 1.2.4 模运算
      *
+     * 1.3 递归简论
+     *
      *
      */
 
+
+    public static void main(String[] args) {
+        fun_1_2_4_mod1();
+    }
+
+    /**
+     * 1.2.4 模运算
+     */
+    public static void fun_1_2_4_mod1(){
+        // 如果N 整除 A - B, 则说A与B模N同余
+        double N = 10;
+        double A = 13;
+        double B = 3;
+        System.out.println(N / (A - B));
+        System.out.println(A % N);
+        System.out.println(B % N);
+
+        // a % b, if a > b, 取余。if a < b, return a
+
+        System.out.println(1 % 100);
+        System.out.println(2 % 100);
+        System.out.println(3 % 100);
+        System.out.println(4 % 100);
+        System.out.println(5 % 100);
+        System.out.println(6 % 100);
+
+        // A / 13 == B / 13 (mod 10)
+        System.out.println(N / ((A / 13) - (B / 13)));
+
+    }
+
+    /**
+     * 1.3的例子，华氏度转摄氏度
+     */
+    public static void fun_1_3_fahrenheit_to_celsius(){
+        // C = 5(F - 32)/9
+    }
 
 
 
